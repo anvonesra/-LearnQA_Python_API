8cb6a48e112531c4be899ad42c0851305ead44ae
anvonesra <irinka89@bk.ru>
2022-11-09 22:16:21 +0300

    Ex5

--- Ex5.py
+++ Ex5.py
@@ -1,12 +1,5 @@
 import json
 
-json_text = '{"messages":[{"message":"This is the first message","timestamp":"2021-06-04 16:40:53"},' \
-                        '{"message":"And this is a second message","timestamp":"2021-06-04 16:41:01"}]}'
+json_text = '{"messages":[{"message":"This is the first message","timestamp":"2021-06-04 16:40:53"},{"message":"And this is a second message","timestamp":"2021-06-04 16:41:01"}]}'
 obj = json.loads(json_text)
-
-key = "messages"
-
-if key in obj:
-    print(obj[key][1])
-else:
-    print(f"Key {key} doesn't exist in JSON")
+print(obj['messages'][1]['message'])
