10e998f5d2a3a0f67c82850637264e69eb876895
anvonesra <irinka89@bk.ru>
2022-11-17 14:55:34 +0300

    Ex10

---
+++ Ex11.py
@@ -0,0 +1,12 @@
+import requests
+
+
+class Test11:
+
+    def test_cookie(self):
+        response = requests.post(
+            "https://playground.learnqa.ru/api/homework_cookie"
+        )
+
+        assert response.cookies['HomeWork'] == 'hw_value', \
+            f"Incorrect response cookie: {response.cookies['HomeWork']}"
